31fb2a2e5a4f7ac66e7f51ed1f2b17b5c9fd8276
ZeroHX <61070023@kmitl.ac.th>
2020-03-21 00:16:00 +0700

    Add another config

---
+++ hostname.py
@@ -0,0 +1,6 @@
+import telnet_router
+router_1 = telnet_router.TN_ROUTER('10.30.7.1', '', 'cisco', 'class')
+router_1.authen()
+router_1.disable_pause()
+router_1.change_hostname("R1s")
+router_1.terminate()
---
+++ showrun.py
@@ -0,0 +1,5 @@
+import telnet_router
+router_1 = telnet_router.TN_ROUTER('10.30.7.1', '', 'cisco', 'class')
+router_1.authen()
+router_1.show_running('')
+router_1.terminate()
